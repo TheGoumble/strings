e35d4141c98a61576fb476f2e6ec0687d9754dec
Javier Vargas <thebruh@Javiers-MacBook-Air.local>
2022-07-14 11:55:55 -0400

    intro to strings

---
+++ strings-basics.js
@@ -0,0 +1,28 @@
+const message = 'Hello Javascript Students' 
+const msgToLowerCase = message.toLowerCase()
+const msgToUpperCase = message.toUpperCase()
+const messageNoSpace = message.trim()
+const newMessage = message.replace('Students', 'Engineers')
+const withSubString = message.substring(9, 14);
+const withStartWith = message.startsWith('Hello');
+const withEndWith = message.endsWith('Students');
+
+console.log(message);
+console.log(msgToLowerCase);
+console.log(msgToUpperCase);
+console.log(messageNoSpace);
+console.log(newMessage);
+console.log(withSubString);
+console.log(withStartWith);
+console.log(withEndWith);
+
+
+const mySchool = 'Boca Code';
+const myIndex = mySchool.indexOf('Code')
+console.log(myIndex);
+
+const myString = 'Hello-level-up-students!';
+const newString = myString.split('-');
+console.log(newString.reverse);
+
+
